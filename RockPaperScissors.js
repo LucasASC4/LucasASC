0d00ef481cb30e5384da10eb5631cf6c47b307e8
LucasASC4 <Chopsticksnyc101@gmail.com>
2017-07-11 14:45:24 -0400

    Added Rock Paper Scissors

---
+++ RockPaperScissors.js
@@ -0,0 +1,10 @@
+var Type = ["Rock", "Paper", "Scissors"];
+
+var choices = ['r','p','s'];
+var compChoice = choices[Math.floor(Math.random()*3)]; 
+
+if (playerChoice == compChoice){
+    console.log("Tie!")
+}
+
+if (playerChoice = 'r' && compChoice 
