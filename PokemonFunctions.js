4eb5a8129c450f9e45a7cfb013ad5abecda4898b
LucasASC4 <Chopsticksnyc101@gmail.com>
2017-07-12 13:38:44 -0400

    added new word, sentence, letter

--- PokemonFunctions.js
+++ PokemonFunctions.js
@@ -5,11 +5,26 @@ var result = Math.floor(randNum);
 return result;
 }
 
-var myAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];{
-    console.log(myAlphabet[rand(26)])
+var myAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+function randletter(){
+    return myAlphabet[rand(26)]
 }
-function rand(num){
+var word = ""
+function randWord(){
+    var wordLength = Math.ceil(Math.random()*10) + 1
+
+for( i=0; i < wordLength; i++){
+    word = word + randletter();
+}
+return word
+}
+console.log(randWord());
+
+var sentence = ""
+
+function randSentence(){
+    var sentenceLength = Math.ceil(Math.random()*10) +1
+for(i=0; i < sentenceLength; i++){
 
     
-}
-var myWord = ["abcdefghijklmnopqrstuvwxyz"];
+}}
